b2ca78a75569aad7a7da162a58994b2e76b642fa
David Denton <denton.david@gmail.com>
2009-02-18 17:37:22 +0000

    dirty - expanded testcases

--- src/main/org/niceassert/ConcreteClassProxyFactory.java
+++ src/main/org/niceassert/ConcreteClassProxyFactory.java
@@ -22,6 +22,18 @@ public class ConcreteClassProxyFactory {
     private ConcreteClassProxyFactory() {
     }
 
+    public <T> T proxyFor(final Class<T> targetClass, final InvocationHandler invocationHandler) {
+        if(!canImposterise(targetClass)) throw new IllegalArgumentException("Can't imposterize ");
+
+        try {
+            setConstructorsAccessible(targetClass, true);
+            Class<T> proxyClass = createProxyClass((Class) targetClass);
+            return (T) ((Class) targetClass).cast(createProxy(proxyClass, invocationHandler));
+        } finally {
+            setConstructorsAccessible(targetClass, false);
+        }
+    }
+
     private static final NamingPolicy NAMING_POLICY_THAT_ALLOWS_IMPOSTERISATION_OF_CLASSES_IN_SIGNED_PACKAGES = new DefaultNamingPolicy() {
         @Override
         public String getClassName(String prefix, String source, Object key, Predicate names) {
@@ -34,18 +46,6 @@ public class ConcreteClassProxyFactory {
             return method.isBridge() ? 1 : 0;
         }
     };
-
-    public <T> T proxyFor(final Class<T> targetClass, final InvocationHandler invocationHandler) {
-        if(!canImposterise(targetClass)) throw new IllegalArgumentException("Can't imposterize ");
-
-        try {
-            setConstructorsAccessible(targetClass, true);
-            Class<T> proxyClass = createProxyClass((Class) targetClass);
-            return (T) ((Class) targetClass).cast(createProxy(proxyClass, invocationHandler));
-        } finally {
-            setConstructorsAccessible(targetClass, false);
-        }
-    }
 
     private boolean canImposterise(final Class targetClass) {
         return !targetClass.isPrimitive() && !Modifier.isFinal(targetClass.getModifiers()) && !toStringMethodIsFinal(targetClass);
@@ -98,7 +98,7 @@ public class ConcreteClassProxyFactory {
         return proxy;
     }
 
-    public static class ClassWithSuperclassToWorkAroundCglibBug {
+    private static class ClassWithSuperclassToWorkAroundCglibBug {
     }
 
 }
---
+++ src/test/org/niceassert/ConcreteClassProxyFactoryTest.java
@@ -0,0 +1,73 @@
+package org.niceassert;
+
+import net.sf.cglib.proxy.InvocationHandler;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+
+public class ConcreteClassProxyFactoryTest {
+
+    private final ConcreteClassProxyFactory concreteClassProxyFactory = ConcreteClassProxyFactory.INSTANCE;
+
+    @Test(expected = IllegalArgumentException.class)
+    public void failsToImposterizeFinalClass() {
+        assertFailsToImposterize(FinalClass.class);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void failsToImposterizeClassWithFinalStringMethod() {
+        assertFailsToImposterize(ClassWithFinalStringMethod.class);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void failsToImposterizePrimitiveClass() {
+        assertFailsToImposterize(Integer.class);
+    }
+
+    @Test
+    public void proxyExtendsTargetObject() {
+        assertThat(ExtendableClass.class.isAssignableFrom(concreteClassProxyFactory.proxyFor(ExtendableClass.class, null).getClass()), is(true));
+    }
+    
+    @Test
+    public void proxyInvokesPassedHandler() {
+        final AtomicBoolean called = new AtomicBoolean(false);
+        concreteClassProxyFactory.proxyFor(ExtendableClass.class, new InvocationHandler() {
+            public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+                called.set(true);
+                return null;
+            }
+        }).toString();
+        assertThat(called.get(), is(true));
+
+        assertThat(ExtendableClass.class.isAssignableFrom(concreteClassProxyFactory.proxyFor(ExtendableClass.class, null).getClass()), is(true));
+    }
+
+    private void assertFailsToImposterize(Class targetClass) {
+        concreteClassProxyFactory.proxyFor(targetClass, new FailingInvocationHandler());
+    }
+
+    private static class FailingInvocationHandler implements InvocationHandler {
+        public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+            throw new AssertionError();
+        }
+    }
+
+    private class ClassWithFinalStringMethod {
+        @Override
+        public final String toString() {
+            return null;
+        }
+    }
+
+    private final class FinalClass {
+    }
+
+    public class ExtendableClass {
+    }
+
+}
