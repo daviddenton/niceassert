9f086925685afa2abb42121bdaefc8644b633e0c
David Denton <denton.david@gmail.com>
2009-02-21 13:59:35 +0000

    dirty - refactored Expected to use different matchers for success and fail

--- src/main/org/niceassert/Expected.java
+++ src/main/org/niceassert/Expected.java
@@ -4,18 +4,45 @@ import net.sf.cglib.proxy.InvocationHandler;
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import static org.junit.Assert.assertThat;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class Expected {
+public class Expected<T> {
 
-    public static <T> ExpectationBuilder<T> expect(final  T target) {
-        return new ExpectationBuilder<T>(target);
+    public static <T> Expected<T> expect(final T target) {
+        return new Expected<T>(target);
     }
 
-    public static Matcher throwException(final Throwable t) {
-        return new BaseMatcher<Throwable>() {
+    private final T target;
+    private Matcher returnMatcher;
+    private Matcher throwsMatcher;
+    private boolean matcherSet;
+
+    private Expected(T target) {
+        this.target = target;
+    }
+
+    public Expected<T> toThrowExceptionThat(Matcher matcher) {
+        validateExpectedBehaviour();
+        this.throwsMatcher = matcher;
+        return this;
+    }
+
+    public Expected<T> toReturnValueThat(Matcher matcher) {
+        validateExpectedBehaviour();
+        this.returnMatcher = matcher;
+        return this;
+    }
+
+    private void validateExpectedBehaviour() {
+        if(matcherSet) throw new IllegalArgumentException("Expected behaviour already set");
+        this.matcherSet = true;
+    }
+
+    public Expected<T> toThrowException(final Throwable t) {
+        toThrowExceptionThat(new BaseMatcher<Throwable>() {
 
             public boolean matches(Object o) {
                 return o.getClass().equals(t.getClass());
@@ -24,11 +51,12 @@ public class Expected {
             public void describeTo(Description description) {
                 description.appendText(t.getClass().getName());
             }
-        };
+        });
+        return this;
     }
 
-    public static <T> Matcher<T> returnValue(final T t) {
-        return new BaseMatcher<T>() {
+    public Expected<T> toReturnValue(final Object t) {
+        toReturnValueThat(new BaseMatcher<T>() {
             public boolean matches(Object o) {
                 return o.equals(t);
             }
@@ -36,43 +64,25 @@ public class Expected {
             public void describeTo(Description description) {
                 description.appendText(" but got: " + t);
             }
-        };
+        });
+        return this;
     }
 
-    public static Matcher returnValueThat(final Matcher matcher) {
-        return matcher;
-    }
+    public T whenCalling() {
 
-    public static Matcher throwExceptionThat(final Matcher matcher) {
-        return matcher;
-    }
-
-    public static class ExpectationBuilder<T> {
-        private final T target;
-        private Matcher matcher;
-
-        ExpectationBuilder(T target) {
-            this.target = target;
-        }
-
-        public ExpectationBuilder<T> to(Matcher matcher) {
-            this.matcher = matcher;
-            return this;
-        }
-
-        public T whenCalling() {
-            return (T) ConcreteClassProxyFactory.INSTANCE.proxyFor(new InvocationHandler() {
-                public Object invoke(Object object, Method method, Object[] args) throws Throwable {
-                    try {
-                        final Object o = method.invoke(target, args);
-                        org.junit.Assert.assertThat(o, matcher);
-                        return o;
-                    } catch (InvocationTargetException e) {
-                        org.junit.Assert.assertThat(e.getTargetException(), matcher);
-                    }
-                    return null;
+        if(!matcherSet) throw new IllegalArgumentException("No expectation set");
+        return (T) ConcreteClassProxyFactory.INSTANCE.proxyFor(new InvocationHandler() {
+            public Object invoke(Object object, Method method, Object[] args) throws Throwable {
+                try {
+                    final Object o = method.invoke(target, args);
+                    assertThat(o, returnMatcher);
+                    return o;
+                } catch (InvocationTargetException e) {
+                    assertThat(e.getTargetException(), throwsMatcher);
                 }
-            }, target.getClass());
-        }
+                return null;
+            }
+        }, target.getClass());
     }
+
 }
--- src/test/org/niceassert/ExpectedTest.java
+++ src/test/org/niceassert/ExpectedTest.java
@@ -8,24 +8,45 @@ import static org.niceassert.Expected.*;
 public class ExpectedTest {
     private static final String STRING = "String";
 
+    @Test (expected = IllegalArgumentException.class)
+    public void whenExceptionThrownAndNoExpectationSet() throws AnException {
+        expect(new AThrowingObject()).whenCalling().aMethod();
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenValueReturnedAndNoExpectationSet() throws AnException {
+        expect(new AReturningObject()).whenCalling().aMethod();
+    }
+
+
+    @Test (expected = IllegalArgumentException.class)
+    public void attemptToSetSameExpectationTwice() throws AnException {
+        expect(new AReturningObject()).toReturnValue(STRING).toReturnValue(STRING).whenCalling().aMethod();
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void attemptToSetCompetingExpectationTwice() throws AnException {
+        expect(new AReturningObject()).toReturnValue(STRING).toThrowException(new AnException()).whenCalling().aMethod();
+    }
+
     @Test
     public void exceptionChecked() throws AnException {
-        expect(new AThrowingObject()).to(throwException(new AnException())).whenCalling().aMethod();
+        expect(new AThrowingObject()).toThrowException(new AnException()).whenCalling().aMethod();
     }
 
     @Test
     public void exceptionCheckedWithCustomMatcher() throws AnException {
-        expect(new AThrowingObject()).to(throwExceptionThat(is(equalTo(new AnException())))).whenCalling().aMethod();
+        expect(new AThrowingObject()).toThrowExceptionThat(is(equalTo(new AnException()))).whenCalling().aMethod();
     }
 
     @Test
     public void returnedValueChecked() {
-        expect(new AReturningObject()).to(returnValue(STRING)).whenCalling().aMethod();
+        expect(new AReturningObject()).toReturnValue(STRING).whenCalling().aMethod();
     }
 
     @Test
     public void returnedValueCheckedUsingCustomAssert() {
-        expect(new AReturningObject()).to(returnValueThat(is(STRING))).whenCalling().aMethod();
+        expect(new AReturningObject()).toReturnValueThat(is(STRING)).whenCalling().aMethod();
     }
 
 
