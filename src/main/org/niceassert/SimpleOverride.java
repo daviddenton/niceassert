843bb69eeb8739b1e8e9df8bc9dbe1b4893c2358
David Denton <denton.david@gmail.com>
2009-03-06 21:50:48 +0000

    dirty - refactor of simpleoverride. towards unification

--- src/main/org/niceassert/SimpleOverride.java
+++ src/main/org/niceassert/SimpleOverride.java
@@ -4,40 +4,38 @@ import net.sf.cglib.proxy.InvocationHandler;
 
 import java.lang.reflect.Method;
 
-public class SimpleOverride {
+public class SimpleOverride<T> {
 
     public static <T> T modifyForOverride(final T target) {
         return (T) ConcreteClassProxyFactory.INSTANCE.proxyFor(new OverridableInvocationHandler(target), target.getClass(), Overrideable.class);
     }
 
-    public static <T> OverrideBuilder<T> override(T target) {
+    public static <T> SimpleOverride<T> override(T target) {
         if (!Overrideable.class.isAssignableFrom(target.getClass()))
             throw new IllegalArgumentException("Target object must be modified for override");
-        return new OverrideBuilder<T>(Overrideable.class.cast(target));
+        return new SimpleOverride<T>(Overrideable.class.cast(target));
     }
 
-    static class OverrideBuilder<T> {
-        private final Overrideable overrideableTarget;
-        private final OverrideInvocationMatcher matcher = new OverrideInvocationMatcher();
+    private final Overrideable overrideableTarget;
+    private final OverrideInvocationMatcher matcher = new OverrideInvocationMatcher();
 
-        public OverrideBuilder(Overrideable overrideableTarget) {
-            this.overrideableTarget = overrideableTarget;
-            overrideableTarget.setMatcher(matcher);
-        }
+    public SimpleOverride(Overrideable overrideableTarget) {
+        this.overrideableTarget = overrideableTarget;
+        overrideableTarget.setMatcher(matcher);
+    }
 
-        public OverrideBuilder<T> to(Action action) {
-            matcher.setAction(action);
-            return this;
-        }
+    public SimpleOverride<T> to(Action action) {
+        matcher.setAction(action);
+        return this;
+    }
 
-        public T whenCalling() {
-            return (T) ConcreteClassProxyFactory.INSTANCE.proxyFor(new InvocationHandler() {
-                public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
-                    matcher.recordInvocation(method, objects);
-                    return null;
-                }
-            }, overrideableTarget.getTarget().getClass());
-        }
+    public T whenCalling() {
+        return (T) ConcreteClassProxyFactory.INSTANCE.proxyFor(new InvocationHandler() {
+            public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
+                matcher.recordInvocation(method, objects);
+                return null;
+            }
+        }, overrideableTarget.getTarget().getClass());
     }
 
     public static Action returnValue(final Object returnValue) {
@@ -79,6 +77,7 @@ public class SimpleOverride {
 
     private static interface Overrideable<T> {
         T getTarget();
+
         void setMatcher(OverrideInvocationMatcher matcher);
     }
 }
