af3fc21fb7456fe563677d7343a1ef9be628bbf7
David Denton <denton.david@gmail.com>
2009-03-16 22:29:25 +0000

    dirty - refactor

--- src/main/org/niceassert/ActionInvocationHandler.java
+++ src/main/org/niceassert/ActionInvocationHandler.java
@@ -13,24 +13,24 @@ class ActionInvocationHandler implements InvocationHandler {
     public Object invoke(Object o, Method method, Object[] objects) throws Throwable {
         try {
             Object result = action.execute(objects);
-            if (returnValueIsCompatible(method.getReturnType(), result)) {
+            if (returnValueIsCompatibleWithMethod(result, method)) {
                 return result;
             }
-            throw new ClassCastException("Can't override method to return incompatible class (expected=" + method.getReturnType() + ", got=" + result + ")");
+            throw new ClassCastException("Can't override method " + method.getName() + " return incompatible class (expected=" + method.getReturnType() + ", got=" + result + ")");
         } catch (Throwable throwable) {
-            if(thrownExceptionIsCompatible(method, throwable)) throw throwable;
+            if(thrownExceptionIsCompatibleWithMethod(throwable, method)) throw throwable;
             throw new ClassCastException("Can't override method " + method.getName() + " to throw incompatible exception " + throwable.getClass());
         }
     }
 
-    private static boolean thrownExceptionIsCompatible(Method method, Throwable throwable) throws Throwable {
+    private static boolean thrownExceptionIsCompatibleWithMethod(Throwable throwable, Method method) throws Throwable {
         for (Class exceptionClass : method.getExceptionTypes()) {
             if (exceptionClass.isAssignableFrom(throwable.getClass())) return true;
         }
         return RuntimeException.class.isAssignableFrom(throwable.getClass());
     }
 
-    private static boolean returnValueIsCompatible(Class<?> expected, Object returnValue) {
-        return returnValue == null || expected.isAssignableFrom(returnValue.getClass());
+    private static boolean returnValueIsCompatibleWithMethod(Object returnValue, Method method) {
+        return returnValue == null || method.getReturnType().isAssignableFrom(returnValue.getClass());
     }
 }
